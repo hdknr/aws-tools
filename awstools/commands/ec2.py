4125453040f1ca7dd4160609fa533c2a4e03fa86
user.name <gmail@hdknr.com>
2024-08-09 10:37:39 +0900

    get_id_from_items

--- awstools/commands/ec2.py
+++ awstools/commands/ec2.py
@@ -25,17 +25,21 @@ def ec2(ctx):
     pass
 
 
-@ec2.command()
-@click.argument("items", nargs=-1)
-@click.pass_context
-def restart_instances(ctx, items):
-    """ID/タグで指定されたインスタンスを再起動する"""
+def get_id_from_items(items):
     ids = [i for i in items if i.startswith("i-")]
     tags = [i for i in items if i.find("=") > 0]
 
     if tags:
         response = ec2_lib.get_instances_tags(tags)
         ids = ids + ec2_lib.get_ids_from_response(response)
+    return ids
 
-    ids = [i for i in items if i.startswith("i-")]
+
+@ec2.command()
+@click.argument("items", nargs=-1)
+@click.pass_context
+def restart_instances(ctx, items):
+    """ID/タグで指定されたインスタンスを再起動する"""
+
+    ids = get_id_from_items(items)
     list(map(ec2_lib.restart, ids))
