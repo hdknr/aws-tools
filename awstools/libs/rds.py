4b158089483c6e397614aaefd67d8513ef0e2aed
user.name <gmail@hdknr.com>
2024-08-09 16:42:18 +0900

    boto3.setup_default_session を呼ばない

--- awstools/libs/rds.py
+++ awstools/libs/rds.py
@@ -1,14 +1,19 @@
 import logging
 import boto3
+import os
 
 
 logger = logging.getLogger()
 
 
+def create_client(region_name=None):
+    return boto3.client("rds")
+
+
 def start(instance_id, client=None):
     # Start
     # https://boto3.amazonaws.com/v1/documentation/api/1.26.86/reference/services/rds/client/start_db_cluster.html
-    client = client or boto3.client("rds")
+    client = client or create_client()
     client.start_db_instance(DBInstanceIdentifier=instance_id)
 
 
@@ -16,19 +21,19 @@ def stop(instance_id, client=None):
     # Stop
     # https://boto3.amazonaws.com/v1/documentation/api/1.26.86/reference/services/rds/client/stop_db_cluster.html
 
-    client = client or boto3.client("rds")
+    client = client or create_client()
     client.stop_db_instance(DBInstanceIdentifier=instance_id)
 
 
 def start_cluster(cluster_id, client=None):
     # Start
     # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds/client/start_db_instance.html
-    client = client or boto3.client("rds")
+    client = client or create_client()
     client.start_db_cluster(DBClusterIdentifier=cluster_id)
 
 
 def stop_cluster(cluster_id, client=None):
     # https://boto3.amazonaws.com/v1/documentation/api/1.26.86/reference/services/rds/client/stop_db_instance.html
 
-    client = client or boto3.client("rds")
+    client = client or create_client()
     client.stop_db_cluster(DBClusterIdentifier=cluster_id)
